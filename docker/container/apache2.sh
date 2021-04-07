370d96c77461fbfea08e1084780eaf6c4681c8f3
crystal.lin <crystal.lin@acer.com>
2021-04-07 15:12:04 +0800

    Ubuntu Env

--- docker/container/apache2.sh
+++ docker/container/apache2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 
+mkdir -p /usr/share/slurm-web/dashboard/web-conf
+cp /etc/slurm-web/clusters.config.json /usr/share/slurm-web/dashboard/web-conf
 exec /usr/sbin/apache2 -D FOREGROUND
--- docker/run/run.sh
+++ docker/run/run.sh
@@ -3,7 +3,7 @@
 # EDIT: indicate where your configuration files will be stored
 data=$PWD
 
-docker run -d -v $data/conf:/etc/slurm-web \
+docker run --restart=always -d -v $data/conf:/etc/slurm-web \
               -v /etc/munge:/etc/munge \
               -v /etc/slurm:/etc/slurm-llnl \
               -v /etc/passwd:/etc/passwd \
